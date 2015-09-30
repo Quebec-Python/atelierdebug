5136407391b4301473b474c0412eecc4f8dda8c2
Yannick Hold <yannickhold@gmail.com>
2015-09-30 01:23:44 +0000

    Added beginner exercice.

--- beginner/ex1.py
+++ beginner/ex1.py
@@ -1,1 +1,16 @@
- 
+
+
+def numericDict(list):
+    """Cree un dictionnaire"""
+    # Cree
+    v = list
+
+    # Cree la liste des clefs
+    k = list(range(len(v)))
+
+    # Genere un dictionnaire avec les clefs et les valeurs
+    return dict(k, v)
+
+
+if __name__ == '__main__':
+    res = numericDict(['a', 'b', 'c']) # Devrait retourner {1: 'a', 2: 'b', 3: 'c'}
